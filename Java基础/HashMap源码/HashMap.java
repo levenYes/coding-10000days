74d2e519b0548fe394ff74f7a44c4f77abbb0e29
liwenye <levenyes@icloud.com>
2018-04-18 20:18:19 +0800

    day 22 2018年4月18日

---
+++ Java基础/HashMap源码/HashMap.java
@@ -0,0 +1,122 @@
+public class HashMap<K,V> extends AbstractMap<K,V>
+        implements Map<K,V>, Cloneable, Serializable {
+    /**
+     * 以上部分省略
+     */
+    void resize(int newCapacity){
+        //引入扩容前的Entry数组
+        Entry[] oldTable = table;
+        int oldCapacity = oldTable.length;
+        //扩容前的数组大小如果已经达到最大，即2的30次方
+        if (oldCapacity == MAXIMUM_CAPACITY){
+            //调整阈值为int的最大值（2的32次方减1），这样以后就不会扩容了
+            threshold = Integer.MAX_VALUE;
+            return
+        }
+
+        Entry[] newTable = new Entry[newCapacity];
+        transfer(newTable);
+        threshold = (int) (newCapacity*loadFactor);
+    }
+
+    void transfer(Entry[] newTable){
+        Entry[] src = table;
+        int newCapacity = newTable.length;
+        for (int j = 0; j < src.length; j++) {
+            Entry<K,V> e = src[j];
+            if(e != null) {
+                src[j] = null;
+                do {
+                    Entry<K,V> next = e.next;
+                    int i = indexFor(e.hash, newCapacity);
+                    e.next = newTable[i];
+                    newTable[i] = e;
+                    e = next;
+                } while (e != null);
+            }
+        }
+    }
+
+    static int indexFor(int h, int length) {
+        return h & (length - 1);
+    }
+    /**
+     * 以上部分为1.7的实现
+     *
+     * 以下部分为1.8的实现
+     */
+    final Node<K,V>[] resize() {
+        Node<K,V>[] oldTab = table;
+        int oldCap = (oldTab == null) ? 0 : oldTab.length;
+        int oldThr = threshold;
+        int newCap, newThr = 0;
+        if (oldCap > 0) {
+            if (oldCap >= MAXIMUM_CAPACITY) {
+                threshold = Integer.MAX_VALUE;
+                return oldTab;
+            }
+            else if ((newCap = oldCap << 1) < MAXIMUM_CAPACITY &&
+                    oldCap >= DEFAULT_INITIAL_CAPACITY)
+                //2倍增长
+                newThr = oldThr << 1;
+        }
+        else if (oldThr > 0)
+            newCap = oldThr;
+        else {
+            newCap = DEFAULT_INITIAL_CAPACITY;
+            newThr = (int)(DEFAULT_LOAD_FACTOR * DEFAULT_INITIAL_CAPACITY);
+        }
+        if (newThr == 0) {
+            float ft = (float)newCap * loadFactor;
+            newThr = (newCap < MAXIMUM_CAPACITY && ft < (float)MAXIMUM_CAPACITY ?
+                    (int)ft : Integer.MAX_VALUE);
+        }
+        threshold = newThr;
+        @SuppressWarnings({"rawtypes","unchecked"})
+        Node<K,V>[] newTab = (Node<K,V>[])new Node[newCap];
+        table = newTab;
+        if (oldTab != null) {
+            for (int j = 0; j < oldCap; ++j) {
+                Node<K,V> e;
+                if ((e = oldTab[j]) != null) {
+                    oldTab[j] = null;
+                    if (e.next == null)
+                        newTab[e.hash & (newCap - 1)] = e;
+                    else if (e instanceof TreeNode)
+                        ((TreeNode<K,V>)e).split(this, newTab, j, oldCap);
+                    else {
+                        Node<K,V> loHead = null, loTail = null;
+                        Node<K,V> hiHead = null, hiTail = null;
+                        Node<K,V> next;
+                        do {
+                            next = e.next;
+                            if ((e.hash & oldCap) == 0) {
+                                if (loTail == null)
+                                    loHead = e;
+                                else
+                                    loTail.next = e;
+                                loTail = e;
+                            }
+                            else {
+                                if (hiTail == null)
+                                    hiHead = e;
+                                else
+                                    hiTail.next = e;
+                                hiTail = e;
+                            }
+                        } while ((e = next) != null);
+                        if (loTail != null) {
+                            loTail.next = null;
+                            newTab[j] = loHead;
+                        }
+                        if (hiTail != null) {
+                            hiTail.next = null;
+                            newTab[j + oldCap] = hiHead;
+                        }
+                    }
+                }
+            }
+        }
+        return newTab;
+    }
+}
